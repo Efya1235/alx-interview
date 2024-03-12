624f1463ec588cf621090f6aea56debc74a7ba84
Efya1235 <bkumah51@gmail.com>
2024-03-12 08:58:14 -0700

    committed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
+# lockboxes
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
+    x = len(boxes)
+    item = [False] * x
+    item[0] = True
+    stack = [0]
 
-    num_boxes = len(boxes)
-    visited = set()
-    queue = [0]
+    while stack:
+        box = stack.pop()
+        for each in boxes[box]:
+            if each >= 0 and each < x and not item[each]:
+                item[each] = True
+                stack.append(each)
 
-    while queue:
-        current_box = queue.pop(0)
-        visited.add(current_box)
-
-        for key in boxes[current_box]:
-            if key < num_boxes and key not in visited:
-                queue.append(key)
-
-    return len(visited) == num_boxes
-
-
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # Output: True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # Output: True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # Output: False
-
+    return all(item)
